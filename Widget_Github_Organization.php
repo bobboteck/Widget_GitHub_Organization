f017ce6959c2c99a389d9891db1272e0ab5ff2b7
bobboteck <bobboteck@gmail.com>
2015-02-28 22:55:32 +0100

    Modificata la gestione del Widget, attualmente non funzionava, questa versione è ancora un test

--- Widget_Github_Organization.php
+++ Widget_Github_Organization.php
@@ -1,31 +1,75 @@
 <?php
 /*
-Plugin Name: Widget GitHub Organization Event Traking 
+Plugin Name: Widget GitHub Organization
 Plugin URI: http://www.officinerobotiche.it/
 Description: This Wordpress Widget show recent GitHub events of a specific Organization
-Version: 0.0.1
+Version: 0.0.2
 Author: Roberto D'Amico
 Author URI: http://www.officinerobotiche.it/
 */
-class GithubOrganizationEventTraking extends WP_Widget {
-    function GithubOrganizationEventTraking() {
-        parent::__construct(
-			// Base ID of your widget
-			'RDA_GOET', 
-			// Widget name will appear in UI
-			__('Officine Robotiche - GithHub Organization Event Traking', 'RDA_GOET_OfficineRobotiche'), 
-			// Widget description
-			array( 'description' => __( 'Widget used to show last event in the Organization GitHub', 'RDA_GOET_OfficineRobotiche' ), ) 
-		);
+class Widget_Github_Organization extends WP_Widget 
+{
+    function Widget_Github_Organization() 
+    {
+		$widget_ops = array('classname' => 'Widget_Github_Organization', 'description' => 'Widget used to show last event in the Organization GitHub' );
+		$this->WP_Widget('Widget_Github_Organization', 'GithHub Organization Event Traking', $widget_ops);
     }
 
-    function widget($args, $instance) {
-		// Widget variables
-		$title = apply_filters( 'widget_title', $instance['title'] );
-		$organization = apply_filters( 'widget_organization', $instance['organization'] );
-		$itemCount = apply_filters( 'widget_itemCount', $instance['itemCount'] );
-        echo $args['before_widget'];
+    function form($instance) 
+    {
+        //$title = esc_attr($instance['title']);
+        
+        /* Impostazioni di default del widget */
+		$defaults = array(
+            'title' => 'GithHub Organization Event Traking',
+            'organization' => '',
+            'itemCount' => 10
+        );
+ 
+		$instance = wp_parse_args((array)$instance, $defaults);
+		$title = $instance['title'];
+		$organization = $instance['organization'];
+		$itemCount = $instance['itemCount'];
+?>
+<p>
+	<label for="<?php echo $this->get_field_id('title');?>">
+	Title: <input class="widefat" id="<?php echo $this->get_field_id('title');?>" name="<?php echo $this->get_field_name('title');?>" type="text" value="<?php echo $title; ?>" />
+	</label>
+</p>
+<p>
+	<label for="<?php echo $this->get_field_id('organization');?>">
+	Organization: <input class="widefat" id="<?php echo $this->get_field_id('organization');?>" name="<?php echo $this->get_field_name('organization');?>" type="text" value="<?php echo $organization; ?>" />
+	</label>
+</p>
+<p>
+	<label for="<?php echo $this->get_field_id('itemCount');?>">
+	Item to show: <input class="widefat" id="<?php echo $this->get_field_id('itemCount');?>" name="<?php echo $this->get_field_name('itemCount');?>" type="text" value="<?php echo $itemCount; ?>" />
+	</label>
+</p>
+<?php
+    }
+
+    function update($new_instance, $old_instance) 
+    {
+        $instance = $old_instance;
+		$instance['title'] = $new_instance['title'];
+		$instance['organization'] = $new_instance['organization'];
+		$instance['itemnum'] = $new_instance['itemnum'];
+		return $instance;
+    }
+
+    function widget($args, $instance) 
+    {
+		extract($args, EXTR_SKIP);
+		
+		echo $before_widget;
+		
+		$title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+		$organization = empty($instance['organization']) ? 'officinerobotiche' : $instance['organization'];
+		$itemCount = empty($instance['itemCount']) ? '10' : $instance['itemCount'];
   
+		if (!empty($title))
+			echo $before_title . $title . $after_title;;
         //INIZIO WIDGET
 ?>
 <link rel="stylesheet" href="octicons/octicons.css">
@@ -141,47 +185,9 @@ function commitUrl(url)
 <?php
         //FINE WIDGET
  
-        echo $args['after_widget'];
-    }
-    
-    function update($new_instance, $old_instance) {
-        return $new_instance;
-    }
-    
-    function form($instance) {
-        //$title = esc_attr($instance['title']);
-        
-        /* Impostazioni di default del widget */
-		$defaults = array(
-            'title' => 'GithHub Organization Event Traking',
-            'organization' => '',
-            'itemnum' => 10
-        );
- 
-		$instance = wp_parse_args((array)$instance, $defaults);
-?>
-<p>
-	<label for="<?php echo $this->get_field_id('title');?>">
-	Title: <input class="widefat" id="<?php echo $this->get_field_id('title');?>" name="<?php echo $this->get_field_name('title');?>" type="text" value="<?php echo $title; ?>" />
-	</label>
-</p>
-<p>
-	<label for="<?php echo $this->get_field_id('organization');?>">
-	Organization: <input class="widefat" id="<?php echo $this->get_field_id('organization');?>" name="<?php echo $this->get_field_name('organization');?>" type="text" value="<?php echo $organization; ?>" />
-	</label>
-</p>
-<p>
-	<label for="<?php echo $this->get_field_id('itemCount');?>">
-	Item to show: <input class="widefat" id="<?php echo $this->get_field_id('itemCount');?>" name="<?php echo $this->get_field_name('itemCount');?>" type="text" value="<?php echo $itemCount; ?>" />
-	</label>
-</p>
-<?php
+        echo $after_widget;
     }
 }
- 
-function load_widget() {
-    register_widget('RDA_GOET');
-}
- 
-add_action('widgets_init', 'load_widget');
+
+add_action( 'widgets_init', create_function('', 'return register_widget("Widget_Github_Organization");') );
 ?>
